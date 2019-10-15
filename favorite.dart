a7c7c8f38b11402a6be0d715ec940becd6f664cb
Rachit Yagnik <rachity@gmail.com>
2019-10-15 11:37:57 +0530

    Add files via upload

---
+++ favorite.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FavoritePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      child: new Center(
+        child: new Text('Favourite Restaurants Page'),
+      ),
+    );
+  }
+}
