f1096add9f8efcf6b82ec180a027b1e070a1a53f
Rachit Yagnik <rachity@gmail.com>
2019-10-15 11:38:17 +0530

    Delete restaurant.dart

--- restaurant.dart
+++
@@ -1,8 +0,0 @@
-class Restaurant {
-  final String name;
-  final String type;
-  final int approxcost;
-  final double rating;
-
-  Restaurant(this.name, this.type, this.approxcost, this.rating);
-}
